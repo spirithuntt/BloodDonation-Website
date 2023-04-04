7ef1f6706b42272ea598360c7b6465f8edb61b95
nouhaila el housni <112879456+spirithuntt@users.noreply.github.com>
2023-04-04 10:01:45 +0000

    refactoring Appointment code

--- blood-donation/app/Http/Controllers/AppointmentController.php
+++ blood-donation/app/Http/Controllers/AppointmentController.php
@@ -2,53 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AppointmentRequest;
 use App\Models\Appointment;
 use App\Models\BusinessHour;
+use App\Services\AppointmentService;
 use Carbon\CarbonPeriod;
 use Illuminate\Http\Request;
 
 class AppointmentController extends Controller
 {
-    //index
     public function index()
     {
         $datePeriod = CarbonPeriod::create(now(), now()->addDays(6));
+
         $appointments = [];
-        foreach ($datePeriod as $date) {
-            $dayName = $date->format('l');
-    
-            $businessHours = BusinessHour::where('day', $dayName)->first();
-            
-            $hours = $businessHours->TimesPeriod;
-            dd($hours);
-            $currentAppointments = Appointment::where('date', $date->toDateString())->pluck('time')->map(function ($time) {
-                return $time->format('H:i');
-            })->toArray();
-            
-            $reservedHours = array_intersect($hours, $currentAppointments);
-            $availableHours = array_diff($hours, $currentAppointments);
-            
-            $appointments[] = [
-                'day_name' => $dayName,
-                'date' => $date->format('d-M'),
-                'full_date' => $date->format('Y-m-d'),
-                'available_hours' => $availableHours,
-                'reserved_hours' => $reservedHours, // add this line
-                'off' => $businessHours->off,
-                'business_hours' => $hours,
-            ];
+
+        foreach($datePeriod as $date){
+            $appointments [] = (new AppointmentService)->generateTimeData($date);
         }
+
         return view('appointments.reserve', compact('appointments'));
     }
 
-
-
-
-
-    public function reserve(Request $request)
+    public function reserve(AppointmentRequest $request)
     {
 
-        $data = $request->merge(['user_id' => auth()->id()])->toArray();
+        $data = $request->merge(['user_id'=>auth()->id()])->toArray();
 
         Appointment::create($data);
 
---
+++ blood-donation/app/Services/AppointmentService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Appointment;
+use App\Models\BusinessHour;
+use Carbon\Carbon;
+
+class AppointmentService {
+
+    //to clean up the code in the controller and make it more readable and reusable 
+    //we can create a service class to handle the logic of the controller
+    public function generateTimeData(Carbon $date)
+    {
+            $dayName = $date->format('l');
+
+            $businessHours = BusinessHour::where('day',$dayName)->first();
+
+            $hours = array_filter($businessHours->TimesPeriod);
+
+            $currentAppointments = Appointment::where('date', $date->toDateString())->pluck('time')->map(function($time){
+                return $time->format('H:i');
+            })->toArray();
+
+           $availbleHours = array_diff($hours,$currentAppointments);
+
+           return [
+                'day_name' => $dayName,
+                'date' => $date->format('d M'),
+                'full_date' => $date->format('Y-m-d'),
+                'available_hours' => $availbleHours,
+                'reserved_hours' => $currentAppointments,
+                'business_hours' => $hours,
+                'off' => $businessHours->off
+            ];
+    }
+}
